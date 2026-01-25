a238a1fd8b8518ce205bdcef4efa39f4ed39d1f2
Lee Yong Min <vbn0213@gmail.com>
2026-01-25 21:07:31 +0900

    FEAT: 헤더 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,34 @@
 import { createRoute } from '@granite-js/react-native';
-import { TextButton } from '@toss/tds-react-native';
-import { SafeArea } from '@toss/tds-react-native/private';
+import { Txt } from '@toss/tds-react-native';
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { colors } from '@toss/tds-colors';
 
 export const Route = createRoute('/', {
   component: Page,
 });
 
 function Page() {
-  const goToAboutPage = () => {};
-
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <View style={{ marginBottom: 24 }}>
+        <Txt typography="t1" fontWeight="bold">
+          말투의 온도,
+        </Txt>
+        <Txt typography="t1" fontWeight="bold">
+          AI가 점검해드릴게요
+        </Txt>
+      </View>
+      <Txt typography="st9" style={{ color: colors.grey700 }}>
+        하루 3회 무료로 대화의 톤을 체크해보세요.
+      </Txt>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+});
