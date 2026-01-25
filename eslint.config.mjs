7933996452b9dbd712353cc16b885b19eb71150f
Lee Yong Min <vbn0213@gmail.com>
2026-01-25 21:07:24 +0900

    CHORE: eslint 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -16,4 +17,25 @@ export default [
     },
   },
   pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off', // unused-imports가 처리
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error', // 미사용 import 제거
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ];
