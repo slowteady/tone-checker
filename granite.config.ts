e13b9e3391390f8713a522812cc52dd706b08004
Lee Yong Min <vbn0213@gmail.com>
2026-01-27 23:26:51 +0900

    CHORE: 세팅 변경

--- granite.config.ts
+++ granite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   plugins: [
     appsInToss({
       brand: {
-        displayName: '말투 체커', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
-        primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-        icon: 'https://static.toss.im/appsintoss/17559/aafab834-619a-44b1-9797-55cbaa173adf.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+        displayName: '이 말투 어때', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+        primaryColor: '#5A5AFF', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+        icon: 'https://static.toss.im/appsintoss/17559/e9702e48-2cfa-4d46-84cc-94e0ed553ba2.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
       },
       permissions: [
         {
