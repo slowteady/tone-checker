fafdb1838265c192e9813a9555301abcd0f360d0
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2026-01-29 17:46:31 +0900

    FEAT: 결과 카드 컴포넌트 추가

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,10 +1,18 @@
-import { Txt } from '@toss/tds-react-native';
-import { View } from 'react-native';
+import { colors, Txt } from '@toss/tds-react-native';
+import { StyleSheet, View } from 'react-native';
 
 export const ResultCard = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Txt>ResultCard</Txt>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: colors.background,
+    borderRadius: 24,
+  },
+});
