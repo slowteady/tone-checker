c09b178bf289c4e6b772e1ee445297aac23df477
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2026-02-09 18:14:49 +0900

    CHORE: 원복

--- granite.config.ts
+++ granite.config.ts
@@ -7,7 +7,7 @@ import { sentry } from '@granite-js/plugin-sentry';
 dotenv.config();
 
 export default defineConfig({
-  scheme: 'tone-checker',
+  scheme: 'intoss',
   appName: 'tone-checker',
   plugins: [
     sentry({ useClient: false }),
